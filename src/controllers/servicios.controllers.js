748570086c24e7e20ac9477c9cdfc3c684b6c12b
Valentina Iramain <valentinairamain2020@gmail.com>
2025-12-23 11:28:16 -0300

    agregamos la logica para editar un producto por ID

--- src/controllers/servicios.controllers.js
+++ src/controllers/servicios.controllers.js
@@ -57,3 +57,20 @@ export const obtenerServicioId = async (req, res) => {
     }
 
 }
+
+//EDITAR por ID
+export const editarServicio = async (req, res) => {
+    try {
+        const servicioBuscado = await Servicio.findById(req.params.id)
+        if (!servicioBuscado) {
+            return res.status(404).json({ mensaje: 'No se encontró un servicio con el ID enviado' })
+        }
+
+        //aqui queremos editar el servicio
+        await Servicio.updateOne({ _id: req.params.id }, req.body)
+        res.status(200).json({ mensaje: "El producto fue actualizado correctamente" })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ mensaje: 'Ocurrió un error al intentar editar un servicio' })
+    }
+}
--- src/routes/servicios.routes.js
+++ src/routes/servicios.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { crearServicio, listarServicios, obtenerServicioId, prueba } from "../controllers/servicios.controllers.js";
+import { crearServicio, editarServicio, listarServicios, obtenerServicioId, prueba } from "../controllers/servicios.controllers.js";
 
 
 const router = Router();
@@ -16,7 +16,7 @@ router.route('/test').get(prueba)
 //contruir la ruta: http://localhost:3000/api/servicios/
 router.route('/').post(crearServicio).get(listarServicios)
 // ruta GET por ID
-router.route('/:id').get(obtenerServicioId)
+router.route('/:id').get(obtenerServicioId).put(editarServicio)
 
 
 
