a36bb62dc2a28d334d9bcaca560c86d23875965b
Valentina Iramain <valentinairamain2020@gmail.com>
2025-12-23 10:10:20 -0300

    agregamos la logica necesaria para crear un servicio en la DB

--- src/controllers/servicios.controllers.js
+++ src/controllers/servicios.controllers.js
@@ -1,10 +1,29 @@
 //contiene la logica para administrar los servicios de programacion
+import Servicio from "../models/servicio.js"
 
+// funcion de test. BORRAR LUEGO DE PROBAR
 export const prueba = (req,res)=>{
     console.log('consulta de prueba')
     res.send('ejemplo de respuesta desde el backend')
 }
 
-export const crearServicio = (req,res)=>{
-   
+
+// CREAR servicio - POST
+export const crearServicio = async (req,res)=>{
+    try{
+        //agregar validacion de datos
+        
+        console.log(req)
+        //req.body van los datos del servicio a crear, en formato json
+        console.log(req.body)
+
+        const servicioNuevo = new Servicio(req.body)
+        //save() es una query de mongoose
+        await servicioNuevo.save()
+        // POST se contesta con 201 (created)
+        res.status(201).json({mensaje:'Servicio creado correctamente'})
+    }catch(error){
+        console.error(error)
+        res.status(500).json({mensaje:'Ocurrió un error al intentar crear un servicio'})
+    }
 }
--- src/routes/servicios.routes.js
+++ src/routes/servicios.routes.js
@@ -7,13 +7,14 @@ const router = Router();
 //aqui diseñamso todas las rutas con los servicios
 // metodos: get - post - put o patch - delete
 
-
 //definir los metodos que se ejecutaran en esta ruta
+//RUTA: test es de prueba (borrar luego de probar)
 router.route('/test').get(prueba)
 
 
-//ALTA
-router.route('/'.post(crearServicio))
+//RUTA ALTA - POST
+//contruir la ruta: http://localhost:3000/api/servicios/
+router.route('/').post(crearServicio)
 
 
 export default router
