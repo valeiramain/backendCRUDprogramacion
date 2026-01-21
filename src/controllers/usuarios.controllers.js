d881995634e0ad895aabd86d1b82b99ef9254ecd
valeiramain <valentinairamain2020@gmail.com>
2026-01-21 08:51:09 -0300

    se crea login del usuario

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,5 +1,5 @@
-import  Usuario  from "../models/usuario.js";
-import bcrypt from "bcrypt";  
+import Usuario from "../models/usuario.js";
+import bcrypt from "bcrypt";
 
 export const crearUsurio = async (req, res) => {
   try {
@@ -7,20 +7,62 @@ export const crearUsurio = async (req, res) => {
 
     //hashear la contraseña
     const saltos = bcrypt.genSaltSync(10);
-    console.log(saltos)
+    console.log(saltos);
     const passwordHasheado = bcrypt.hashSync(req.body.password, saltos);
     req.body.password = passwordHasheado;
 
     const usuarioNuevo = new Usuario(req.body);
     await usuarioNuevo.save();
     res.status(201).send("Usuario creado correctamente");
-
   } catch (error) {
     console.error(error);
     res.status(500).send("Ocurrió un error al intentar crear usuario");
   }
 };
 
-export const listarUsurios = (req, res) => {
-  res.send("listar usuario");
+export const listarUsurios = async (req, res) => {
+  try {
+    const listaUsuarios = await Usuario.find();
+    if (listaUsuarios.length === 0) {
+      return res.status(404).json({ mensaje: "No hay usuarios registrados" });
+    }
+    res.status(200).json(listaUsuarios);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrió un error al intentar listar los usuarios" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    // toma email y password del body req.body.email, req.body.password
+    const { email, password } = req.body;
+    //verificar si el email existe en la base de datos
+    const usuarioBuscado = await Usuario.findOne({ email }); // await Usuario.findOne({ email: req.body.email });
+   
+    if (!usuarioBuscado) {
+      return res.status(404).json({ mensaje: "No se encontró al usuario" });
+    }
+    
+    // verificar la contraseña que envía el usuario con la que está en la base de datos
+    const passwordValido = bcrypt.compareSync(password,usuarioBuscado.password);
+    
+    if (!passwordValido) {
+      // el codigo de error 401 es de no autorizado
+      return res.status(401).json({ mensaje: "Credenciales Incorrectas" });
+    }
+    
+    // Si todo correcto, informar al front que debe loguear al usuario
+
+    //agregar JWT
+    res.status(200).json({ mensaje: "Inicio de sesión exitoso",nombre: usuarioBuscado.nombre });
+
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrió un error al intentar iniciar sesión" });
+  }
 };
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { crearUsurio, listarUsurios } from "../controllers/usuarios.controllers.js";
+import { crearUsurio, listarUsurios,login } from "../controllers/usuarios.controllers.js";
 
 const router = Router();
 
@@ -7,5 +7,6 @@ const router = Router();
 //contruir la ruta: http://localhost:3000/api/usuarios
  
 router.route("/").get(listarUsurios).post(crearUsurio);
+router.route("/login").post(login);
 
 export default router;
